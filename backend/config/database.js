4a9603c04d9375e6e583ab88c02d4ba381077003
Aryan Singh <as10071999@gmail.com>
2024-04-21 12:03:39 +0530

    Added env

---
+++ backend/config/database.js
@@ -0,0 +1,11 @@
+import * as mysql from "mysql"; // Import the entire mysql module
+
+const pool = mysql.createPool({
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.MYSQL_DB,
+});
+
+export default pool; // Export the pool using the default export syntax
